3a2b95397d4e3713873a4287952ef922d3c781c7
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-05 21:10:34 +0900

    マスタッシュ（二重の中括弧）記法

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ let state = { count: 0 }
 let app = new Vue({
   el: "#app",
   data: { 
-    newTodoText: '',
-    visitCount: 0,
-    hideCompletedTodos: false,
-    todos: [],
-    error: null
+    message: 'Vue js!'
   }
 })
-
-state.count++
