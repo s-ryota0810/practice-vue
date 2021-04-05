25a6644c73adbeedee48adfcd3da8d981e475cf1
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-05 21:57:09 +0900

    methodsの使い方

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ let app = new Vue({
   el: "#app",
   data: { 
     message: 'Vue js!',
-    scroll: 0
+    scroll: 0,
+    count: 0
   },
   mounted: function() {
     this.scroll = 100
+  },
+  
+  methods: {
+    increment: function() {
+      this.count += 1
+    }
   }
 })
