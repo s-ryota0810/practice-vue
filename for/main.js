c2b63992a340f6f1a8c06bccd890ad0466c440d5
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-06 23:33:07 +0900

    リストに要素を追加

--- for/main.js
+++ for/main.js
@@ -1,10 +1,25 @@
 let app = new Vue({
   el: "#app",
   data: {
+    name: 'キマイラ',
     list: [
       {id: 1, name: 'スライム', hp: 100},
       {id: 2, name: 'ゴブリン', hp: 200},
       {id: 3, name: 'ドラゴン', hp: 500}
     ]
+  },
+  
+  methods: {
+    doAdd: function() {
+      let max = this.list.reduce(function(a,b) {
+        return a > b.id ? a : b.id
+      },0)
+      
+      this.list.push({
+        id: max + 1,
+        name: this.name,
+        hp: 500
+      })
+    }
   }
 })
