2515d380560b3d118d66de290b7acca9de57486e
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-07 00:15:57 +0900

    文字列に対するv-for

--- for/main.js
+++ for/main.js
@@ -6,7 +6,8 @@ let app = new Vue({
       {id: 1, name: 'スライム', hp: 100},
       {id: 2, name: 'ゴブリン', hp: 200},
       {id: 3, name: 'ドラゴン', hp: 500}
-    ]
+    ],
+    text: 'Vue'
   },
   
   created: function() {
