f323ef8e929f31eae4b738c1195d0c9815bbc2db
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-07 23:11:01 +0900

    targetプロパティはよくわからない

--- eventHandling/main.js
+++ eventHandling/main.js
@@ -1,12 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    count: 0
+    count: 0,
+    message:  "Hello Vue"
   },
   
   methods: {
     handleClick: function(){
       alert('test')
+    },
+    
+    handleInput: function(event) {
+      alert(this.message)
+      this.message = event.target.value
     }
   }
   
