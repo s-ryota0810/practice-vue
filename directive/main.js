52fb64f5ee6ebe748eac27fc0dbf774a042f7d3e
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-07 22:01:25 +0900

    物理DOM操作の$refsの特性を確認

--- directive/main.js
+++ directive/main.js
@@ -1,6 +1,16 @@
 new Vue({
   el: "#app",
-  mounted: function(){
-    console.log(this.$refs.hello)
-  }
+  data: {
+    show: true
+  },
+  
+  methods: {
+    handleClick(){
+      let count = this.$refs.count
+      if (count) {
+        count.innerText = parseInt(count.innerText,10) + 1
+      }
+    }
+  },
+  
 })
