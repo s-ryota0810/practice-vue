877c11e1b00f564f1f50b23bec5137cb7e3660c0
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-07 22:27:44 +0900

    HTMLエンティティをせずそのまま表示させるv-html

--- controll-template/main.js
+++ controll-template/main.js
@@ -2,10 +2,11 @@ let app = new Vue({
   el: "#app",
   data: {
     message: 'React',
+    msg: 'Hello <strong>Vue.js!<strong>',
     url: 'https://www.google.com'
   },
   
   mounted: function(){
-    this.message = 'Vue'
+    this.message = 'Hello Vue'
   }
 })
