8c8b784421bcf13befdb80571cf3d4dcafc6d93e
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-07 21:46:50 +0900

    外部からデータを取得する

--- for/main.js
+++ for/main.js
@@ -1,41 +1,14 @@
 let app = new Vue({
   el: "#app",
   data: {
-    name: 'キマイラ',
-    list: [
-      {id: 1, name: 'スライム', hp: 100},
-      {id: 2, name: 'ゴブリン', hp: 200},
-      {id: 3, name: 'ドラゴン', hp: 500}
-    ],
-    text: 'Vue'
+    list: []
   },
   
   created: function() {
-    this.list.forEach(function(item) {
-      this.$set(item, 'active', false)
-    }, this)
-  },
-  
-  methods: {
-    doAdd: function() {
-      let max = this.list.reduce(function(a,b) {
-        return a > b.id ? a : b.id
-      },0)
-      
-      this.list.push({
-        id: max + 1,
-        name: this.name,
-        hp: 500
-      })
-    },
-    
-    doRemove: function(index) {
-      this.list.splice(index,1)
-    },
-    
-    doAttack: function(index) {
-      this.list[index].hp -= 10
-    }
-    
+    axios.get('list.json').then(function(response){
+      this.list = response.data
+    }.bind(this)).catch(function(e){
+      console.error(e)
+    })
   }
 })
