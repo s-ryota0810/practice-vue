ad700701449177ea395ab05d8f97b43ed31cacb8
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-26 23:32:19 +0900

    算出プロパティを組み合わせる

--- section16/main.js
+++ section16/main.js
@@ -2,11 +2,23 @@ new Vue({
   
   el: '#app',
   data: {
-    width: 800
+    width: 800,
+    height: 600
   },
+  
   computed: {
     halfWidth: function(){
       return this.width / 2
+    },
+    halfHeight: function(){
+      return this.height / 2
+    },
+    
+    halfPoint: function(){
+      return {
+        x: this.halfWidth,
+        y: this.halfHeight
+      }
     }
   }
 })
