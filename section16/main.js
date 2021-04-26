1de2448b9f4fe090fe7f0f411da197060f1089c5
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-27 00:28:38 +0900

    絞り込み機能の実装

--- section16/main.js
+++ section16/main.js
@@ -2,7 +2,16 @@ new Vue({
   
   el: '#app',
   data: {
-    width: 800
+    width: 800,
+    budget: 300,
+    limit: 2,
+    list: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 400 },
+      { id: 4, name: 'オレンジ', price: 300 },
+      { id: 5, name: 'メロン', price: 500 },
+    ]
   },
   
   computed: {
@@ -13,6 +22,16 @@ new Vue({
     
     computedData: function(){
       return Math.random()
+    },
+    
+    matched: function(){
+      return this.list.filter(function(el){
+        return el.price <= this.budget
+      }, this)
+    },
+    
+    limited: function(){
+      return this.matched.slice(0, this.limit)
     }
   },
   
