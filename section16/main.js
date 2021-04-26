756fb2863c3d6e2864fcb334e144bebeb8870939
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-26 23:41:30 +0900

    getとsetの使い方

--- section16/main.js
+++ section16/main.js
@@ -2,23 +2,13 @@ new Vue({
   
   el: '#app',
   data: {
-    width: 800,
-    height: 600
+    width: 800
   },
   
   computed: {
-    halfWidth: function(){
-      return this.width / 2
-    },
-    halfHeight: function(){
-      return this.height / 2
-    },
-    
-    halfPoint: function(){
-      return {
-        x: this.halfWidth,
-        y: this.halfHeight
-      }
+    halfWidth: {
+      get: function(){ return this.width / 2 },
+      set: function(val){ this.width = val * 2 }
     }
   }
 })
