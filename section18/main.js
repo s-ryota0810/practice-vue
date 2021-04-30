1b61b8b216f54ba5cd18e363e142bedd4256138f
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-30 23:39:25 +0900

    グローバルフィルタの設定

--- section18/main.js
+++ section18/main.js
@@ -1,12 +1,10 @@
-new Vue({
+Vue.filter('localeNum', function(val){
+  return val.toLocaleString()
+})
+
+let vue = new Vue({
   el: '#app',
   data: {
     price: 19800
-  },
-  
-  filters: {
-    localeNum: function(val) {
-      return val.toLocaleString()
-    }
   }
 });
