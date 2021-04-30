3755e0a5e4df426bb1f48ba965c3a92608836933
ryota.s.0810 <akaashi0810@gmail.com>
2021-04-30 23:46:43 +0900

    引数が複数のフィルター

--- section18/main.js
+++ section18/main.js
@@ -1,10 +1,18 @@
-Vue.filter('localeNum', function(val){
-  return val.toLocaleString()
-})
-
 let vue = new Vue({
   el: '#app',
   data: {
-    price: 19800
+    price: 19800,
+    message: 'Vue',
+    foo: '='
+  },
+  
+  filters: {
+    LocaleNum: function(val) {
+      return val.toLocaleString()
+    },
+    
+    filter: function(message, foo, num) {
+      console.log(message, foo, num)
+    }
   }
 });
