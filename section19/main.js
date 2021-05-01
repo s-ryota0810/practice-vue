b408d2a44e9b1974cb064cd9b34c2e0780850f35
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-01 21:40:54 +0900

    動画を再生するディレクティブ

--- section19/main.js
+++ section19/main.js
@@ -1,6 +1,14 @@
 let vue = new Vue({
   el: '#app',
+  data: {
+    video1: false
+  },
+  
   directives: {
+    video(el, binding) {
+      binding.value ? el.play() : el.pause()
+    },
+    
     focus: {
       inserted: function(el){
         el.focus()
