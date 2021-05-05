4602095d773503e116ef4978d944ffce3877aaf7
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-06 04:05:22 +0900

    .syncによる双方向データバインディング

--- section23/main.js
+++ section23/main.js
@@ -1,63 +1,31 @@
-Vue.component('comp-child', {
-  template: '<button v-on:click="handleClick">イベント発火</button>',
-  methods: {
-    handleClick: function(){
-      this.$emit('childs-event')
+Vue.component('my-component', {
+  template: '<div class="my-component">\
+    <p>名前.{{ name }} HP.{{ hp }}</p>\
+    <p>名前 <input v-model="localName"></p>\
+    <p>HP <input size="5" v-model.number="localHp"></p>\
+    </div>',
+    props: {
+      name: String,
+      hp: Number
+    },
+    computed: {
+      localName: {
+        get: function(){ return this.name },
+        set: function(val){ this.$emit('update:name', val) }
+      },
+      localHp: {
+        get: function(){ return this.hp },
+        set: function(val){ this.$emit('update:hp', val)}
+      }
     }
-  }
-  
-});
-
-Vue.component('child-comp', {
-  template: '<li>{{ name }} HP.{{ hp }}\
-    <button v-on:click="doAttack">攻撃する</button></li>',
-  props: { id: Number, name: String, hp: Number },
-  methods: {
-    doAttack: function() {
-      this.$emit('attack', this.id)
-    }
-  }
 })
 
 new Vue ({
   el: "#app",
   data: {
-    list: [
-      { id: 1, name: 'スライム', hp:100 },
-      { id: 2, name: 'ゴブリン', hp:200 },
-      { id: 3, name: 'ドラゴン', hp:500 }
-    ]
-  },
-  methods: {
-    parentsMethod: function(){
-      alert('イベントをキャッチ！')
-    },
-    
-    handleAttack: function(id) {
-      let item = this.list.find(function(el) {
-        return el.id === id
-      })
-      
-      if (item !== undefined && item.hp > 0) item.hp -= 10
-    }
+    name: 'スライム',
+    hp: 100
   }
   
 });
 
-let bus = new Vue ({
-  data: {
-    count: 0
-  }
-})
-
-Vue.component('component-b', {
-  template: '<p>bus: {{ bus.count }}</p>',
-  computed: {
-    bus: function() { return bus.$data }
-  },
-  created: function() {
-    bus.$on('bus-event', function(){
-      this.count++
-    })
-  }
-})
