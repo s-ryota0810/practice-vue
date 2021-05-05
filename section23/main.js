014f188a862b8723959856e1848863737c68f074
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-05 21:28:00 +0900

    no message

--- section23/main.js
+++ section23/main.js
@@ -8,6 +8,17 @@ Vue.component('comp-child', {
   
 });
 
+Vue.component('child-comp', {
+  template: '<li>{{ name }} HP.{{ hp }}\
+    <button v-on:click="doAttack">攻撃する</button></li>',
+  props: { id: Number, name: String, hp: Number },
+  methods: {
+    doAttack: function() {
+      this.$emit('attack', this.id)
+    }
+  }
+})
+
 new Vue ({
   el: "#app",
   data: {
@@ -20,7 +31,33 @@ new Vue ({
   methods: {
     parentsMethod: function(){
       alert('イベントをキャッチ！')
+    },
+    
+    handleAttack: function(id) {
+      let item = this.list.find(function(el) {
+        return el.id === id
+      })
+      
+      if (item !== undefined && item.hp > 0) item.hp -= 10
     }
   }
   
+});
+
+let bus = new Vue ({
+  data: {
+    count: 0
+  }
+})
+
+Vue.component('component-b', {
+  template: '<p>bus: {{ bus.count }}</p>',
+  computed: {
+    bus: function() { return bus.$data }
+  },
+  created: function() {
+    bus.$on('bus-event', function(){
+      this.count++
+    })
+  }
 })
