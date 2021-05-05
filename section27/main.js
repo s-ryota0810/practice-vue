75d49501ab899bf0e58d32d683641891fd92a22d
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-06 04:48:41 +0900

    共通処理をMixinで登録

--- section27/main.js
+++ section27/main.js
@@ -1,10 +1,27 @@
+let mixin = {
+  created: function(){
+    this.hello()
+  },
+  
+  methods: {
+    hello: function(){
+      console.log("Hello from mixin")
+    }
+  }
+}
+
+
 Vue.component('my-component-a', {
+  mixins: [mixin],
   template: '<div class="my-component-a">Component A</div>'
-})
+});
 
 Vue.component('my-component-b', {
+  mixins: [mixin],
   template: '<div class="my-component-b">Component B</div>'
-})
+});
+
+
 
 new Vue ({
   el: "#app",
@@ -16,7 +33,6 @@ new Vue ({
     component: function(){
       return this.componentTypes[this.current]
     }
-  }
-  
+  }    
 });
 
