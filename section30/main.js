12424212a52d42aa6861a0fcf8aa561787a07be6
ryota.s.0810 <akaashi0810@gmail.com>
2021-05-11 23:58:56 +0900

    移動トランジション

---
+++ section30/main.js
@@ -0,0 +1,19 @@
+new Vue ({
+  el: "#app",
+  data: {
+    order: false,
+    list: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 300 }
+    ]
+    
+  },
+  
+  computed: {
+    sortedList: function() {
+      return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+    }
+  }
+});
+
